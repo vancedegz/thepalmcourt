e8d932672b28979850a4c885b406a827cfb19ac6
unknown <engr.vancedegz@gmail.com>
2026-06-24 21:18:58 +0800

    fix: change app port to 3001 to avoid conflict with existing server app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     exec_mode: "cluster",
     env: {
       NODE_ENV: "production",
-      PORT: 3000,
+      PORT: 3001,
     },
     env_production: {
       NODE_ENV: "production",
-      PORT: 3000,
+      PORT: 3001,
     },
     error_file: "./logs/err.log",
     out_file: "./logs/out.log",
